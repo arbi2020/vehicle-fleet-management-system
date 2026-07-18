c93da79d8e1545d04cb882de6162e4c3dd3eb854
arbi2020 <tahabacha2014@gmail.com>
2026-07-18 18:31:49 -0400

    Add abstract Vehicle class

---
+++ src/model/Vehicle.java
@@ -0,0 +1,101 @@
+package model;
+
+import interfaces.Maintainable;
+import interfaces.Rentable;
+
+public abstract class Vehicle implements Rentable, Maintainable {
+
+    protected String id;
+    protected String brand;
+    protected String model;
+    protected int year;
+    protected double mileage;
+    protected boolean available;
+
+
+    public Vehicle(String id, String brand, String model,
+                   int year, double mileage) {
+
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.available = true;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+
+    public double getMileage() {
+        return mileage;
+    }
+
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+
+    @Override
+    public void rent() {
+        available = false;
+    }
+
+
+    @Override
+    public void returnVehicle() {
+        available = true;
+    }
+
+
+    public void addMileage(double distance) {
+        mileage += distance;
+    }
+
+
+    @Override
+    public void performMaintenance() {
+        System.out.println(
+            "Maintenance completed for vehicle " + id
+        );
+    }
+
+
+    @Override
+    public boolean needsMaintenance() {
+        return mileage > 50000;
+    }
+
+
+    public abstract String getVehicleType();
+
+
+    public abstract double calculateRentalCost(int days);
+
+
+    @Override
+    public String toString() {
+
+        return "ID: " + id +
+                ", Type: " + getVehicleType() +
+                ", Brand: " + brand +
+                ", Model: " + model +
+                ", Year: " + year +
+                ", Mileage: " + mileage +
+                ", Available: " + available;
+    }
+}
